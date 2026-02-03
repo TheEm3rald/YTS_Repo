242c48758606bf564284d2fe1c62dbaf6c3b90d9
TheEm3rald <127610844+TheEm3rald@users.noreply.github.com>
2026-02-02 21:52:59 -0500

    Delete TODO page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,23 +35,22 @@ export default function App() {
     client.models.Todo.delete({ id })
   }
           
-  return (
+   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-                onClick={() => deleteTodo(todo.id)}
-        ))}
-      </ul>
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
+      </ul> 
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">Review next step of this tutorial.</a>
       </div>
     </main>
-  );
+  )
 }
